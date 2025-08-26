584f56cf44677cff953a32c0b824aed818b8481b
Infant Kaviya <infantkaviya@gmail.com>
2025-08-26 22:47:15 +0530

    basic commit

--- utils/QuizService.jsx
+++ utils/QuizService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-	baseURL: "http://localhost:9192/api/quizzes"
+	baseURL: "https://quizonline-api.onrender.com/api/quizzes"
 })
 
 export const createQuestion = async(quizQustion) =>{
